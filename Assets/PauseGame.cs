4e29456cc54e85ce00d9880cb169262d0fd248e9
BarbereauM <BARBEREAU.MAXIME@GMAIL.COM>
2020-08-15 00:16:10 +0200

    V1

---
+++ Assets/PauseGame.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour
+{
+    private bool Pause = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Pause = !Pause;
+
+            if(Pause)
+            {
+                Time.timeScale = 0;
+            }
+            else
+            {
+                Time.timeScale = 1;
+            }
+        }
+    }
+}
---
+++ Assets/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    private float startTime;
+    public Text timerText;
+    void Start()
+    {
+        startTime = Time.time;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Reset");
+            
+            startTime = Time.time - Time.time;
+        }
+
+        float t = Time.time - startTime;
+
+        string minutes = ((int) t / 60).ToString();
+        string seconds = (t % 60).ToString("f2");
+        
+        timerText.text = minutes + ":" + seconds;
+    }
+
+    void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+        }
+    
+}
